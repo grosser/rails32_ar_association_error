8d3a48281de4f13ef6e0e69792e76ebe504c44ba
grosser <grosser.michael@gmail.com>
2013-06-17 13:42:31 -0700

    models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_many :parts
+
+  def add_part
+    parts << Part.new(:car => self)
+  end
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,10 @@
 class Part < ActiveRecord::Base
-  attr_accessible :car_id
+  attr_accessible :car_id, :car
+  belongs_to :car
+
+  before_create :foo
+
+  def foo
+    puts car.object_id
+  end
 end
