47d5b119e04584595cc272719d6d31ed10311d8a
grosser <grosser.michael@gmail.com>
2013-06-27 17:41:16 -0700

    patch

--- script/debug_first.rb
+++ script/debug_first.rb
@@ -1,4 +1,13 @@
 require './config/environment'
 
+# PATCH
+#ActiveRecord::Associations::CollectionAssociation.class_eval do
+#  def first_or_last_with_no_new(type, *args)
+#    return if args.first.is_a?(Hash) && owner.new_record?
+#    first_or_last_without_no_new(type, *args)
+#  end
+#  alias_method_chain :first_or_last, :no_new
+#end
+
 Contract.create!
 puts Car.new.parts.first(:order => "id desc") ? "FAIL" : "SUCCESS"
